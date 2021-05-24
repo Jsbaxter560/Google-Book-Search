6dc88436fd9fc132fab3ed2e65b6c376e947b7d7
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-05-23 20:57:09 -0400

    updated frontend content for Search Page,Saved, and Home....working on updating books search for user.

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/api/google", function (req, res) {
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
